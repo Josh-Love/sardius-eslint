cab6f6e9586b655860ac6c4b3b480c29638ee82b
Josh <josh@sardius.media>
2018-08-22 10:26:24 -0500

    Remove Array Deconstructering

--- base.js
+++ base.js
@@ -20,7 +20,23 @@ module.exports = {
       }
     }],
     "padded-blocks": ["error", { "classes": "always" }],
-    "no-trailing-spaces": 0
+    "no-trailing-spaces": 0,
+    "prefer-destructuring": [
+      "error",
+      {
+        "VariableDeclarator": {
+          "array": true,
+          "object": true
+        },
+        "AssignmentExpression": {
+          "array": false,
+          "object": false
+        }
+      },
+      {
+        "enforceForRenamedProperties": false
+      }
+    ]
   },
 
   "parserOptions": {
